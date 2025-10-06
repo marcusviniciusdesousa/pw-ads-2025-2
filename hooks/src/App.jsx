152833a62cf1b37f2936bd77c22b5312b19ebc04
Marcus Alves <marcus291005@gmail.com>
2025-10-06 08:07:29 -0300

    (03/10) React Hooks: exercício 01

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,9 +1,33 @@
+import React from 'react'
 import './App.css'
+
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
+
+import Home from './Home'
+import Exercicio01 from './exercicios/01.jsx'
+import Exercicio02 from './exercicios/02.jsx'
 
 function App() {
 
   return (
     <>
+      <h1>Exercícios React Hooks</h1>
+      <BrowserRouter>
+        <ul>
+          <li> <Link to="/">Página inicial</Link> </li>
+          <li> <Link to="/01">Exercício 01</Link> </li>
+          <li> <Link to="/02">Exercício 02</Link> </li>
+        </ul>
+        
+        <hr />
+
+        <Routes>
+          {/* path="/" significa a raiz do website */}
+          <Route path="/" element={<Home />} />
+          <Route path="/01" element={<Exercicio01 />} />
+          <Route path="/02" element={<Exercicio02 />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ hooks/src/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return <>
+    <p>Bem-vindo(a) ao projeto React Hooks. Clique em algum dos <em>links</em> acima para acessar um exercício.</p>
+  </>
+}
