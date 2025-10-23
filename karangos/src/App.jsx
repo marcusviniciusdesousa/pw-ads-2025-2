c57255d6ef18e287fb004552d05f5363c2b17713
Marcus Alves <marcus291005@gmail.com>
2025-10-23 10:51:38 -0300

    (23/10) Início do projeto 'karangos'

---
+++ karangos/src/App.jsx
@@ -0,0 +1,17 @@
+// import './App.css'
+
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+ return (
+   <>
+     <BrowserRouter>
+    
+     </BrowserRouter>
+   </>
+
+ )
+}
+
+
+export default App
